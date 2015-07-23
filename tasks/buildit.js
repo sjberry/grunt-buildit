233ad79dd1fc7a8a14e00c2f724b5ec3ec3c5d9f
Steven Berry <steven@sberry.me>
2015-07-23 18:03:26 -0400

    Updating to accomodate new version of `buildit`.

--- tasks/buildit.js
+++ tasks/buildit.js
@@ -3,15 +3,14 @@ var buildit = require('buildit');
 
 function task(grunt) {
 	grunt.registerMultiTask('buildit', 'A grunt task for converting CommonJS code to AMD-compliant bundles.', function() {
-		var data, dest, files, options, out;
+		var data, dest, options, out;
 
 		options = this.options();
 		data = this.data;
 
-		files = data.src || data.files || [];
 		dest = data.out || data.dest;
 
-		out = buildit(files, options);
+		out = buildit(options);
 
 		if (dest) {
 			grunt.file.write(dest, out);
